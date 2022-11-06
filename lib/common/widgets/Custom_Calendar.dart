78af92eb2aebb9cee1b46e76cd16ced36a453087
Atanu-Sabyasachi <jena.atanusabyasachi@gmail.com>
2022-11-06 19:49:44 +0530

    ...

--- lib/common/widgets/Custom_Calendar.dart
+++ lib/common/widgets/Custom_Calendar.dart
@@ -36,109 +36,102 @@ class _CustomCalendarState extends State<CustomCalendar> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            //To Show Current Date
-            Container(
-                height: 30,
-                margin: EdgeInsets.only(left: 10),
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  '${selectedDate.day}-${listOfMonths[selectedDate.month - 1]}, ${selectedDate.year}',
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w800,
-                      color: CustomColor.darkBlue),
-                )),
-            SizedBox(height: 10),
-            //To show CustomCalendar Widget
-            SizedBox(
-              height: 80,
-              child: ListView.separated(
-                separatorBuilder: (BuildContext context, int index) {
-                  return SizedBox(width: 10);
+    return Column(
+      children: [
+        //To Show Current Date
+        Container(
+            height: 30,
+            margin: EdgeInsets.only(left: 10),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              '${selectedDate.day}-${listOfMonths[selectedDate.month - 1]}, ${selectedDate.year}',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w800,
+                  color: CustomColor.darkBlue),
+            )),
+        SizedBox(height: 10),
+        //To show CustomCalendar Widget
+        SizedBox(
+          height: 80,
+          child: ListView.separated(
+            separatorBuilder: (BuildContext context, int index) {
+              return SizedBox(width: 10);
+            },
+            itemCount: 90,
+            controller: scrollController,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (BuildContext context, int index) {
+              return InkWell(
+                onTap: () {
+                  setState(() {
+                    currentDateSelectedIndex = index;
+                    selectedDate = DateTime.now().add(Duration(days: index));
+                    widget.onDateTimeChanged!(selectedDate);
+                  });
                 },
-                itemCount: 90,
-                controller: scrollController,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (BuildContext context, int index) {
-                  return InkWell(
-                    onTap: () {
-                      setState(() {
-                        currentDateSelectedIndex = index;
-                        selectedDate =
-                            DateTime.now().add(Duration(days: index));
-                        widget.onDateTimeChanged!(selectedDate);
-                      });
-                    },
-                    child: Container(
-                      height: 80,
-                      width: 80,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(17),
-                          color: currentDateSelectedIndex == index
-                              ? CustomColor.darkBlue
-                              : Colors.grey[200]),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            listOfMonths[DateTime.now()
-                                        .add(Duration(days: index))
-                                        .month -
-                                    1]
-                                .toString(),
-                            style: TextStyle(
-                                fontSize: 16,
-                                color: currentDateSelectedIndex == index
-                                    ? Colors.white
-                                    : Colors.grey),
-                          ),
-                          SizedBox(
-                            height: 2,
-                          ),
-                          Text(
-                            DateTime.now()
-                                .add(Duration(days: index))
-                                .day
-                                .toString(),
-                            style: TextStyle(
-                                fontSize: 22,
-                                fontWeight: FontWeight.w700,
-                                color: currentDateSelectedIndex == index
-                                    ? Colors.white
-                                    : Colors.grey),
-                          ),
-                          SizedBox(
-                            height: 2,
-                          ),
-                          Text(
-                            listOfDays[DateTime.now()
-                                        .add(Duration(days: index))
-                                        .weekday -
-                                    1]
-                                .toString(),
-                            style: TextStyle(
-                                fontSize: 16,
-                                color: currentDateSelectedIndex == index
-                                    ? Colors.white
-                                    : Colors.grey),
-                          ),
-                        ],
+                child: Container(
+                  height: 80,
+                  width: 80,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(17),
+                      color: currentDateSelectedIndex == index
+                          ? CustomColor.darkBlue
+                          : Colors.grey[200]),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        listOfMonths[DateTime.now()
+                                    .add(Duration(days: index))
+                                    .month -
+                                1]
+                            .toString(),
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: currentDateSelectedIndex == index
+                                ? Colors.white
+                                : Colors.grey),
                       ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        DateTime.now()
+                            .add(Duration(days: index))
+                            .day
+                            .toString(),
+                        style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                            color: currentDateSelectedIndex == index
+                                ? Colors.white
+                                : Colors.grey),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        listOfDays[DateTime.now()
+                                    .add(Duration(days: index))
+                                    .weekday -
+                                1]
+                            .toString(),
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: currentDateSelectedIndex == index
+                                ? Colors.white
+                                : Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
         ),
-      ),
-    ));
+      ],
+    );
   }
 }
